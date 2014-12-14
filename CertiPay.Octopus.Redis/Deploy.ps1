af0396b08f9dadda7af0b91b47d1630e048d7825
Matt Wagner <mattgwagner@gmail.com>
2014-12-14 14:29:39 -0500

    Initial revision for redis flush

---
+++ CertiPay.Octopus.Redis/Deploy.ps1
@@ -0,0 +1,20 @@
+﻿###
+### These variables should be set in the Octopus Depoy UI
+###
+
+if (!$RedisServer) { $RedisServer = 'localhost:6379' }
+if (!$RedisDatabaseId) { $RedisDatabaseId = 0 }
+# if (!$RedisPassword) { $RedisPassword = '' }
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+$ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Add-Type -Path "$ScriptPath\StackExchange.Redis.dll"
+
+$ConnectionMultiplexer = [StackExchange.Redis.ConnectionMultiplexer]::Connect($RedisServer + ",allowAdmin=true", $null)
+
+$Server = $ConnectionMultiplexer.GetServer($RedisServer, [object]$null)
+
+$Server.FlushDatabase($RedisDatabaseId, [StackExchange.Redis.CommandFlags]::None)
---
+++ README.md
@@ -0,0 +1,11 @@
+# CertiPay.Octopus
+
+## Description
+
+A collection of Octopack-able packages that Octopus Deploy can use to perform useful functions.
+
+This is for things that can't be accomplished with just a PowerShell script that can be part of the UI modules.
+
+## Usage
+
+## Contributing
