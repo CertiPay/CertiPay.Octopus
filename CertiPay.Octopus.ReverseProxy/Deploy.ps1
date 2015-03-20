64c3d51daa0e6a55ae3a22711be123b1211dd7c3
Matt Wagner <mattgwagner@gmail.com>
2015-03-20 13:12:13 -0400

    Add package for deploying a reverse proxy configuration

---
+++ CertiPay.Octopus.ReverseProxy/Deploy.ps1
@@ -0,0 +1,28 @@
+﻿###
+### These variables should be set in the Octopus Depoy UI
+###
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+$ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+## Ensure Chocolatey is installed
+
+if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall"))
+{
+	Write-Output "Chocolatey Not Found, Installing..."
+	iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1')) 
+}
+
+## Check if webpi.cmd is installed
+
+choco install webpi.cmd
+
+## Check if Application Request Routing 3.0 is installed via webpi
+
+choco install ARRv3_0 -source webpi
+
+## Tell IIS / UrlRewrite to include original host headers
+
+& c:\Windows\system32\inetsrv\AppCmd.exe set config -section:system.webServer/proxy /preserveHostHeader:"True" /commit:apphost
